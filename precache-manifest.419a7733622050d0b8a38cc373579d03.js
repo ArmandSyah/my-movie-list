898b86cda783593ae3cf921a5aa9fdad9ac66428
Armand Syahtama <armand.syahtama@gmail.com>
2019-04-02 21:45:35 -0400

    Updates

---
+++ precache-manifest.419a7733622050d0b8a38cc373579d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "130702c25c8f18b7ef18",
+    "url": "/my-movie-list/static/js/runtime~main.276be067.js"
+  },
+  {
+    "revision": "a1f2e9cef2fc4dee986e",
+    "url": "/my-movie-list/static/js/main.f37bb564.chunk.js"
+  },
+  {
+    "revision": "ffd540f532fe4c1cc49c",
+    "url": "/my-movie-list/static/js/2.9043285c.chunk.js"
+  },
+  {
+    "revision": "a1f2e9cef2fc4dee986e",
+    "url": "/my-movie-list/static/css/main.4f1abbd3.chunk.css"
+  },
+  {
+    "revision": "ffd540f532fe4c1cc49c",
+    "url": "/my-movie-list/static/css/2.efe65d0a.chunk.css"
+  },
+  {
+    "revision": "56bb3b12b0c0898a6daa61234bd4ad6f",
+    "url": "/my-movie-list/index.html"
+  }
+];
